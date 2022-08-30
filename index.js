bb17f5bc7dc11da0087fb4cebbf8b4662374431a
T81oub <93501885+T81oub@users.noreply.github.com>
2022-08-30 04:01:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+const thmasButton = document.querySelector('#thmas-button');
+
+
+thmasButton.addEventListener('click', clickButton);
+
+function clickButton() {
+var URL = "https://docs.google.com/forms/d/e/1FAIpQLSeO33KADL7IbUscXWKkjlFYj5R5Uvz9VdbUeFjG1yJKVay7w/viewform?usp=sf_link";
+
+var win = window.open(URL, "_blank");
+}
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+
+
+
